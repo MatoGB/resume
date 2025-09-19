add51f5971c027486f2b21143c34603fbeafe4e5
MatoGB <matiasg2955@gmail.com>
2025-09-19 17:31:18 -0300

    Arreglos

--- utils.js
+++ utils.js
@@ -4,4 +4,20 @@ function cambiarIdioma(idioma) {
     document.querySelectorAll("." + idioma).forEach(el => el.classList.add("active"));
 }
 
+function mostrarPDF(nombre) {
+    if (nombre == "VOXY") {
+        document.getElementById("pdfFrame").src = "data/" + nombre + ".png";
+        document.getElementById("visor").style.display = "block";
+    }
+    else{
+        document.getElementById("pdfFrame").src ="data/" + nombre + ".pdf";
+    document.getElementById("visor").style.display = "block";
+}
+    
+}
+
+function cerrarPDF() {
+    document.getElementById("visor").style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", () => cambiarIdioma("esp"))
